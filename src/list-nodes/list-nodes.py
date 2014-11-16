22a1d212903f8890a7e7264d9325b56fe4a80e4a
Víctor García Guillén <vgarcia@tcpsi.es>
2014-11-16 09:55:49 +0100

    Added first version of server listing

--- src/list-nodes/list-nodes.py
+++ src/list-nodes/list-nodes.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from keystoneclient.auth.identity import v2
+from keystoneclient               import session
+from novaclient.client            import Client
+
+USER        = ''
+PASS        = ''
+TENANT_ID   = ''
+TENANT_NAME = ''
+AUTH_URL    = ''
+
+NOVA_VERSION = '2'
+
+auth = v2.Password(auth_url    = AUTH_URL,
+				   username    = USER,
+				   password    = PASS,
+				   tenant_name = TENANT_NAME,)
+
+sess = session.Session(auth=auth, verify=False)
+nova = Client(NOVA_VERSION, session=sess, insecure=True)
+
+for server in nova.servers.list():
+	print "Server name: " + Server.name + "; server Id: " + server.id
+
