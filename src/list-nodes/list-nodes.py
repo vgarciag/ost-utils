dcded09d18242ede1870288729ed6e1a06ec7a54
Víctor García Guillén <vgarcia@tcpsi.es>
2014-11-21 16:40:52 +0100

    Added function

--- src/list-nodes/list-nodes.py
+++ src/list-nodes/list-nodes.py
@@ -14,18 +14,22 @@ AUTH_URL    = ''
 
 NOVA_VERSION = '2'
 
-try:
-	auth = v2.Password(auth_url    = AUTH_URL,
-					   username    = USER,
-					   password    = PASS,
-					   tenant_name = TENANT_NAME,)
+def main():
+	try:
+		auth = v2.Password(auth_url    = AUTH_URL,
+						   username    = USER,
+						   password    = PASS,
+						   tenant_name = TENANT_NAME,)
 
-	sess = session.Session(auth=auth, verify=False)
-	nova = Client(NOVA_VERSION, session=sess, insecure=True)
+		sess = session.Session(auth=auth, verify=False)
+		nova = Client(NOVA_VERSION, session=sess, insecure=True)
 
-	for server in nova.servers.list():
-		print "Server name: " + server.name + "; server Id: " + server.id
-except AuthorizationFailure, e:
-	print e
-except e:
-	print "Unkown exception: " + str(e)
+		for server in nova.servers.list():
+			print "Server name: " + server.name + "; server Id: " + server.id
+	except AuthorizationFailure, e:
+		print e
+	except e:
+		print "Unkown exception: " + str(e)
+
+if __name__ == "__main__":
+	main()
