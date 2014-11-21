ce41a7dcaa35d877eb00fea4fddc7c7beada75ac
Víctor García Guillén <vgarcia@tcpsi.es>
2014-11-21 14:55:45 +0100

    some minor changes

--- src/list-nodes/list-nodes.py
+++ src/list-nodes/list-nodes.py
@@ -4,6 +4,7 @@
 from keystoneclient.auth.identity import v2
 from keystoneclient               import session
 from novaclient.client            import Client
+from keystoneclient.openstack.common.apiclient.exceptions import AuthorizationFailure
 
 USER        = ''
 PASS        = ''
@@ -13,14 +14,18 @@ AUTH_URL    = ''
 
 NOVA_VERSION = '2'
 
-auth = v2.Password(auth_url    = AUTH_URL,
-				   username    = USER,
-				   password    = PASS,
-				   tenant_name = TENANT_NAME,)
+try:
+	auth = v2.Password(auth_url    = AUTH_URL,
+					   username    = USER,
+					   password    = PASS,
+					   tenant_name = TENANT_NAME,)
 
-sess = session.Session(auth=auth, verify=False)
-nova = Client(NOVA_VERSION, session=sess, insecure=True)
+	sess = session.Session(auth=auth, verify=False)
+	nova = Client(NOVA_VERSION, session=sess, insecure=True)
 
-for server in nova.servers.list():
-	print "Server name: " + Server.name + "; server Id: " + server.id
-
+	for server in nova.servers.list():
+		print "Server name: " + server.name + "; server Id: " + server.id
+except AuthorizationFailure, e:
+	print e
+except e:
+	print "Unkown exception: " + str(e)
