512a70f092e7bafa7f7c0bcaf8f847875692fc09
Víctor García Guillén <vgarcia@tcpsi.es>
2014-11-22 09:35:54 +0100

    Some Changes more

--- src/list-nodes/list-nodes.py
+++ src/list-nodes/list-nodes.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import sys
 from keystoneclient.auth.identity import v2
 from keystoneclient               import session
 from novaclient.client            import Client
+
 from keystoneclient.openstack.common.apiclient.exceptions import AuthorizationFailure
+from keystoneclient.openstack.common.apiclient.exceptions import EndpointNotFound
 
 USER        = ''
 PASS        = ''
@@ -14,7 +17,10 @@ AUTH_URL    = ''
 
 NOVA_VERSION = '2'
 
-def main():
+def list_nodes ():
+	'''
+	This function list all nodes in tenant
+	'''
 	try:
 		auth = v2.Password(auth_url    = AUTH_URL,
 						   username    = USER,
@@ -25,11 +31,26 @@ def main():
 		nova = Client(NOVA_VERSION, session=sess, insecure=True)
 
 		for server in nova.servers.list():
-			print "Server name: " + server.name + "; server Id: " + server.id
-	except AuthorizationFailure, e:
-		print e
-	except e:
-		print "Unkown exception: " + str(e)
+			print 'Server name: ' + server.name + '; server Id: ' + server.id
 
-if __name__ == "__main__":
+	except AuthorizationFailure as e:
+		print e
+
+	except EndpointNotFound as e:
+		print 'No keyston endpoint found: ' + AUTH_URL + str(e)
+
+	except (KeyboardInterrupt, SystemExit) as e:
+		print 'Keyboard interruptionor system exit. ' + str(e)
+
+	except:
+		print "Unrecognized exception: ", sys.exc_info()[0]
+
+
+def main():
+	'''
+	This function only calls the functions that list nodes
+	'''
+	list_nodes()
+
+if __name__ == '__main__':
 	main()
